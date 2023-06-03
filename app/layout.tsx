f564ebc1722902f595b15c0795841e139dba367f
m-bohra8999 <murtaza.bohra8999@gmail.com>
2023-06-03 13:41:13 +0530

    Initial commit

---
+++ app/layout.tsx
@@ -0,0 +1,17 @@
+import './globals.css';
+import { Nunito } from 'next/font/google';
+
+const nunito = Nunito({ subsets: ['latin'] });
+
+export const metadata = {
+  title: 'Airbnb',
+  description: 'Airbnb Clone',
+};
+
+export default function RootLayout({ children }: { children: React.ReactNode }) {
+  return (
+    <html lang='en'>
+      <body className={nunito.className}>{children}</body>
+    </html>
+  );
+}
