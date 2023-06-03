2cdcf85f4b8da8984c15d98812f0768f6fa1435e
m-bohra8999 <murtaza.bohra8999@gmail.com>
2023-06-03 14:17:54 +0530

    Feat: Navbar UI

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,6 +1,8 @@
 import './globals.css';
 import { Nunito } from 'next/font/google';
 
+import Navbar from './components/Navbar/Navbar';
+
 const nunito = Nunito({ subsets: ['latin'] });
 
 export const metadata = {
@@ -11,7 +13,10 @@ export const metadata = {
 export default function RootLayout({ children }: { children: React.ReactNode }) {
   return (
     <html lang='en'>
-      <body className={nunito.className}>{children}</body>
+      <body className={nunito.className}>
+        <Navbar />
+        {children}
+      </body>
     </html>
   );
 }
